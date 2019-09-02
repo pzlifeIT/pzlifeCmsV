9d0e3129beac6c191e609fa30d4e037258838994
wujunjie <917436062@qq.com>
2019-09-02 11:05:31 +0800

    表格提示信息

--- src/components/page/goods/tabble.vue
+++ src/components/page/goods/tabble.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="tcontent">
     <div class="table-header clearfix">
-      <el-breadcrumb class="breadcrumb"  separator-class="el-icon-arrow-right">
+      <el-breadcrumb class="breadcrumb" separator-class="el-icon-arrow-right">
         <el-breadcrumb-item>商品管理</el-breadcrumb-item>
         <el-breadcrumb-item>商品表格列表</el-breadcrumb-item>
       </el-breadcrumb>
       <el-button class="add fr" type="primary" icon="el-icon-plus" @click="showCard()">添加表格模板</el-button>
-  </div>
+    </div>
 
     <el-table :data="tabbleList" border style="width: 100%">
       <el-table-column prop="id" label="ID" width="50"></el-table-column>
-      <el-table-column  prop="name" label="表格名称"  width="100"></el-table-column>
-      <el-table-column  prop="options" label="模板列表" ></el-table-column>
-      <el-table-column  prop="create_time" width="200" label="创建时间" ></el-table-column>
-      <el-table-column fixed="right" label="操作" width="200" >
+      <el-table-column prop="name" label="表格名称" width="100"></el-table-column>
+      <el-table-column prop="options" label="模板列表"></el-table-column>
+      <el-table-column prop="create_time" width="200" label="创建时间"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="200">
         <template slot-scope="scope">
           <el-button type="primary" size="small" @click="getSheetInfo(scope.row.id)">编辑</el-button>
         </template>
@@ -21,131 +21,134 @@
     </el-table>
     <v-pagination @pageChange="pageChange" :num='num' :total="total" :page="page"></v-pagination>
 
-    <v-card name='添加商品表格模板' width="110" :cardStatus="cardStatus" :ruleType="ruleType" :ruleForm="ruleForm" :rules="rules" @sumbit="sumbit" @hideCard="hideCard"></v-card>
+    <v-card name='添加商品表格模板' width="110" :cardStatus="cardStatus" :ruleType="ruleType" :ruleForm="ruleForm"
+            :rules="rules" @sumbit="sumbit" @hideCard="hideCard"></v-card>
 
   </div>
 </template>
 
 <script>
 
-import vPagination from '../../component/pagination'
-import vCard from '../../component/card'
-export default {
-  data(){
-    return {
-      num:1,
-      cardStatus:false,
-      ruleForm:{},
-      rules:['name','options'],
-      ruleType:{
-        'name':{
-          type:'input',
-          label:'表格名称',
-          placeholder:'请输入表格名称'
+  import vPagination from '../../component/pagination'
+  import vCard from '../../component/card'
+
+  export default {
+    data() {
+      return {
+        num: 1,
+        cardStatus: false,
+        ruleForm: {},
+        rules: ['name', 'options'],
+        ruleType: {
+          'name': {
+            type: 'input',
+            label: '表格名称',
+            placeholder: '请输入表格名称'
+          },
+          'options': {
+            type: 'select',
+            label: '表格模板列表',
+            placeholder: '请选择表格模板',
+            multiple: true,
+            val: 'id',
+            lab: 'title',
+            option: []
+          }
         },
-        'options':{
-          type:'select',
-          label:'表格模板列表',
-          placeholder:'请选择表格模板',
-          multiple:true,
-          val:'id',
-          lab:'title',
-          option:[]
-        }
-      },
-      screen:{
-        page:1,
-        page_num:10
-      },
-      page:1,
-      tabbleList:[],
-      total:0
-    }
-  },
-  components:{
+        screen: {
+          page: 1,
+          page_num: 10
+        },
+        page: 1,
+        tabbleList: [],
+        total: 0
+      }
+    },
+    components: {
       vPagination,
       vCard
-  },
-  mounted(){
-    this.getTabbleList()
-    this.getTemplate()
-  },
-  methods: {
-    showCard(){
+    },
+    mounted() {
+      this.getTabbleList()
+      this.getTemplate()
+    },
+    methods: {
+      showCard() {
         this.ruleForm = {}
         this.cardStatus = true
-    },
-    hideCard(){
-      this.cardStatus = false
-    },
-    getTemplate(){
-      this.$request({
-        url: 'goods/getSheetOption',
-        success:(res)=>{
-          this.ruleType['options'].option = res.options || []
-        }
-      })
-    },
-    sumbit(data = {}){
-      data.ruleForm.options = data.ruleForm.options.join(',')
-      data.ruleForm.id?this.editSheet(data.ruleForm):this.addSheet(data.ruleForm)
-    },
-    editSheet(ruleForm){
-      this.$request({
-        data: ruleForm,
-        url: 'goods/editSheet',
-        form:3,
-        success:(res)=>{
-          this.ruleForm = {}
-          this.getTabbleList()
-          this.cardStatus = false
-        }
-      })
-    },
-    addSheet(ruleForm){
-      this.$request({
-        data: ruleForm,
-        url: 'goods/addSheet',
-        form:1,
-        success:(res)=>{
-          this.ruleForm = {}
-          this.getTabbleList()
-          this.cardStatus = false
-        }
-      })
-    },
-    getSheetInfo(id){
-      this.$request({
-        data: {
-          id:id
-        },
-        url: 'goods/getSheetInfo',
-        success:(res)=>{
-          res.sheet.options = res.sheet.options.map((val)=>{
-            return val.id
-          })
-          this.ruleForm = res.sheet
-          this.cardStatus = true
-        }
-      })
-    },
-    pageChange(obj){
+      },
+      hideCard() {
+        this.cardStatus = false
+      },
+      getTemplate() {
+        this.$request({
+          url: 'goods/getSheetOption',
+          success: (res) => {
+            this.ruleType['options'].option = res.options || []
+          }
+        })
+      },
+      sumbit(data = {}) {
+        data.ruleForm.options = data.ruleForm.options.join(',')
+        data.ruleForm.id ? this.editSheet(data.ruleForm) : this.addSheet(data.ruleForm)
+      },
+      editSheet(ruleForm) {
+        this.$request({
+          data: ruleForm,
+          url: 'goods/editSheet',
+          form: 3,
+          success: (res) => {
+            this.ruleForm = {}
+            this.getTabbleList()
+            this.cardStatus = false
+          },
+
+        })
+      },
+      addSheet(ruleForm) {
+        this.$request({
+          data: ruleForm,
+          url: 'goods/addSheet',
+          form: 1,
+          success: (res) => {
+            this.ruleForm = {}
+            this.getTabbleList()
+            this.cardStatus = false
+          }
+        })
+      },
+      getSheetInfo(id) {
+        this.$request({
+          data: {
+            id: id
+          },
+          url: 'goods/getSheetInfo',
+          success: (res) => {
+            res.sheet.options = res.sheet.options.map((val) => {
+              return val.id
+            })
+            this.ruleForm = res.sheet
+            this.cardStatus = true
+          }
+        })
+      },
+      pageChange(obj) {
         this.screen.page = obj.page
         this.getTabbleList()
-    },
-    getTabbleList(){
-      let that =this;
-      that.$request({
-        data: that.screen,
-        url: 'goods/getSheet',
-        success(res){
-          that.tabbleList = res.sheetlist
-          that.total = res.total || 0;
-        }
-      })
+      },
+      getTabbleList() {
+        let that = this;
+        that.$request({
+          data: that.screen,
+          url: 'goods/getSheet',
+          success(res) {
+            that.tabbleList = res.sheetlist
+            that.total = res.total || 0;
+          }
+        })
+      }
     }
   }
-}
 </script>
 
 <style scoped>
